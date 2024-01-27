1f4700044c0852f5ecf978a7b08b2a3d4366c25a
piiped <aomkadang@gmail.com>
2024-01-27 18:44:24 +0700

    sec4 without animation

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ tlSplitGreat.from(chars, {
   stagger: 0.02,
 }, "+=0");
 
-gsap.registerPlugin(ScrollTrigger);
 
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.set(".wrapper_2", {yPercent:0});
 gsap.to(".wrapper_2", {
@@ -28,19 +28,9 @@ gsap.to(".wrapper_2", {
   scrollTrigger: {
     trigger: ".wrapper_2",
     start: "top 30%", 
-    end: "bottom 10%", 
+    end: "bottom ", 
     scrub: 2
   }
-});
-
-gsap.set(".titleBurrowing", { yPercent: -10});
-gsap.to(".titleBurrowing", {
-  yPercent: -50,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".titleBurrowing",
-    scrub: 2
-  }, 
 });
 
 gsap.set(".bag", { yPercent: 50});
@@ -52,7 +42,7 @@ gsap.to(".bag", {
     scrub: 3
   }
 });
-gsap.set(".hanger", { yPercent: 50});
+gsap.set(".hanger", { yPercent: 20});
 gsap.to(".hanger", {
   yPercent: -50,
   ease: "none",
@@ -61,61 +51,28 @@ gsap.to(".hanger", {
     scrub: 3
   }
 });
-gsap.set(".search", { yPercent: -10});
+gsap.set(".search", { yPercent: 0});
 gsap.to(".search", {
-  yPercent: 50,
+  yPercent: 100,
   ease: "none",
   scrollTrigger: {
     trigger: ".search",
-    scrub: 3
+    start: "top 30%", 
+    end: "bottom 10%", 
+    scrub: 2
   }
 });
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-ScrollTrigger.normalizeScroll(true)
-
-// create the smooth scroller FIRST!
-let smoother = ScrollSmoother.create({
-  smooth: 2,
-  effects: true,
-  normalizeScroll: true
-});
-
-// pin box-c when it reaches the center of the viewport, for 300px
-ScrollTrigger.create({
-  trigger: ".search",
-  pin: true,
-  start: "center center",
-  end: "+=300",
-  markers: true
-});
-
-
-// gsap.set('.search_icon', { scale: 1 });
-
-// gsap.to(".search_icon", {
-//   scale: 15,
-//   ease: "power2",
-//   xPercent: 250, 
-//   yPercent: -500, 
-//   transformOrigin: "right top", 
-//   scrollTrigger: {
-//     trigger: ".search_icon",
-//     start: "top 50%", 
-//     end: "bottom", 
-//     scrub: 1,
-//   }, 
-// });
 
 gsap.set(".shirt-1", { yPercent: 50});
 gsap.set(".shirt-2", { yPercent: 40});
 gsap.set(".shirt-3", { yPercent: 50});
 gsap.set(".shirt-5", { yPercent: 40});
-gsap.set(".shirt-6", { yPercent: 50});
+gsap.set(".shirt-4", { yPercent: 50});
 gsap.set(".shirt-7", { yPercent: 40});
 gsap.set(".shirt-8", { yPercent: 50});
 gsap.set(".shirt-9", { yPercent: 40});
+gsap.set(".shirt-10", { yPercent: 40});
+gsap.set(".shirt-6", { yPercent: 30});
 gsap.to(".shirt-1", {
   yPercent: -50,
   ease: "none",
@@ -150,11 +107,11 @@ gsap.to(".shirt-5", {
     scrub: 4
   }, 
 });
-gsap.to(".shirt-6", {
+gsap.to(".shirt-4", {
   yPercent: -40,
   ease: "none",
   scrollTrigger: {
-    trigger: ".shirt-img-6",
+    trigger: ".shirt-img-4",
     scrub: 3
   }, 
 });
@@ -182,20 +139,123 @@ gsap.to(".shirt-9", {
     scrub: 3
   }, 
 });
-gsap.set(".shirt-4", { yPercent: 50});
-gsap.to(".shirt-4", {
-  xPercent: -100, 
-  yPercent: 250,
-  rotation:-10*1,
-  transformOrigin: "right top", 
+gsap.to(".shirt-10", {
+  yPercent: -60,
   ease: "none",
   scrollTrigger: {
-    trigger: ".shirt-img-4",
-    start: "top 10%", 
-    end: "bottom", 
-    scrub: 2
+    trigger: ".shirt-img-10",
+    scrub: 3
   }, 
 });
 
+gsap.to(".shirt-6", {
+  xPercent: -130, 
+  yPercent: 300,
+  rotation:-20*1,
+  scale:1.5,
+  transformOrigin: "right top", 
+  ease: "none",
+  normalizeScroll: true,
+  scrollTrigger: {
+    trigger: ".shirt-img-6",
+    start: "center 10%", 
+    end: "+=400", 
+    scrub: 2,
+    duration:2,
+  }, 
+});
+gsap.set(".text-1", { yPercent: 160 });
+gsap.to(".text-1", {
+  yPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".text-1",
+    scrub: 1
+  },
+});
+gsap.set(".line-1", { yPercent: 70 });
+gsap.to(".line-1", {
+  yPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".line-1",
+    scrub: 1
+  },
+});
+
+gsap.set(".text-2", { yPercent: 120});
+gsap.to(".text-2", {
+  yPercent: -20,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".text-2",
+    scrub: 1
+  },
+});
+gsap.set(".line-2", { yPercent: 70 });
+gsap.to(".line-2", {
+  yPercent: -20,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".line-2",
+    scrub: 1
+  },
+});
+
+gsap.set(".text-3", { yPercent: 150});
+gsap.to(".text-3", {
+  yPercent: -20,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".text-3",
+    scrub: 1
+  },
+});
+gsap.set(".line-3", { yPercent: 50 });
+gsap.to(".line-3", {
+  yPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".line-3",
+    scrub: 1
+  },
+});
 
 
+gsap.set(".text-4", { yPercent: 110});
+gsap.to(".text-4", {
+  yPercent: -120,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".text-4",
+    scrub: 1
+  },
+});
+gsap.set(".line-4", { yPercent: 80 });
+gsap.to(".line-4", {
+  yPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".line-4",
+    scrub: 1
+  },
+});
+
+gsap.set(".text-5", { yPercent: 30});
+gsap.to(".text-5", {
+  yPercent: -280,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".text-5",
+    scrub: 1
+  },
+});
+gsap.set(".line-5", { yPercent: 30 });
+gsap.to(".line-5", {
+  yPercent: -110,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".line-5",
+    scrub: 1
+  },
+});

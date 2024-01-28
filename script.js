989b2f4ff69578fd76acefcdb4a503282997835a
piiped <aomkadang@gmail.com>
2024-01-29 02:28:17 +0700

    sec5 a little bit

--- script.js
+++ script.js
@@ -259,3 +259,55 @@ gsap.to(".line-5", {
     scrub: 1
   },
 });
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".c_1", {
+  xPercent:90,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".c_1",
+    scrub: 1,
+  },
+});
+gsap.to(".c_2", {
+  xPercent:-100,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".c_2",
+    scrub: 2,
+  },
+});
+gsap.to(".c_3", {
+  xPercent:120,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".c_3",
+    scrub: 4,
+  },
+});
+gsap.to(".c_4", {
+  xPercent:-250,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".c_4",
+    scrub: 5,
+  },
+});
+gsap.to(".btf", {
+  xPercent:-250,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".btf",
+    scrub: 2,
+  },
+});
+
+gsap.set(".cloud_cover", { yPercent: 20});
+gsap.to(".cloud_cover", {
+  yPercent: -20,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".cloud_cover",
+    scrub: 3
+  }
+});

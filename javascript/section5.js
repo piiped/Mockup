4ada3d7ca47e975d3eff21a1d411902e9e9f2c5a
piiped <aomkadang@gmail.com>
2024-03-09 00:36:59 +0700

    fixed something, but I forgot it

--- javascript/section5.js
+++ javascript/section5.js
@@ -1,29 +1,128 @@
 gsap.set(".sec-5-text", { yPercent : -10});
 gsap.to(".sec-5-text", {
-  yPercent: 90,
+  yPercent: 250,
   ease: "none",
   scrollTrigger: {
     trigger: ".sec-5-text",
     start: "center 5%", 
-    end: "+=300", 
+    end: "+=800", 
     scrub: 2,
     duration:2,
+    markers: true
   }, 
 });
 
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.to(".box3", {
+  scale: 2,
+  transformOrigin: "center center",
+  ease: "power1.inOut",
+  scrollTrigger: {
+    trigger: ".box1",
+    start: "top top",
+    end: "+=300",
+    markers: true,
+    scrub: 3
+  }
+});
 
-// const parallaxImage = document.querySelector('.parallax-image');
+//zoom
+gsap.to(".box3", {
+  scale: 7,
+  transformOrigin: "center center",
+  ease: "power1.inOut",
+  immediateRender: false,
+  scrollTrigger: {
+    trigger: ".box2",
+    start: "top top",
+    end: "+=300",
+    markers: true,
+    scrub: 3
+  }
+});
 
-// window.addEventListener('scroll', () => {
-//   const scrollTop = window.pageYOffset;
-//   const imageY = scrollTop * 0.5;
-
-//   parallaxImage.style.transform = `translate(-50%, ${imageY}px)`;
-
-//   if (scrollTop > 50) {
-//     parallaxImage.classList.add('active');
-//   } else {
-//     parallaxImage.classList.remove('active');
+//zoom
+// gsap.to(".group", {
+//   scale: 2,
+//   transformOrigin: "left left",
+//   ease: "power1.inOut",
+//   scrollTrigger: {
+//     trigger: ".group",
+//     start: "top top",
+//     end: "+=300",
+//     markers: true,
+//     scrub: 0.5
 //   }
 // });
+
+// //zoom
+// gsap.to(".group", {
+//   scale: 7,
+//   transformOrigin: "center center",
+//   ease: "power1.inOut",
+//   immediateRender: false,
+//   scrollTrigger: {
+//     trigger: ".group",
+//     start: "center 50%",
+//     end: "+=300",
+//     markers: true,
+//     scrub: 0.5
+//   }
+// });
+
+// gsap.set(".group", { yPercent : 0});
+// gsap.to(".group", {
+//   xPercent: 0, 
+//   yPercent: -10,
+//   scale: 1.5,
+//   transformOrigin: "left top", 
+//   ease: "none",
+//   normalizeScroll: true,
+//   scrollTrigger: {
+//     trigger: ".group",
+//     start: "top 10%", 
+//     end: "top 15%", 
+//     scrub: 1,
+//     duration:10,
+//     markers: true
+//   }, 
+// });
+
+
+// gsap.set(".parallax-image", { yPercent : 0});
+// gsap.to(".parallax-image", {
+//   xPercent: 0, 
+//   yPercent: -10,
+//   scale: 1.5,
+//   transformOrigin: "left top", 
+//   ease: "none",
+//   normalizeScroll: true,
+//   scrollTrigger: {
+//     trigger: ".parallax-image",
+//     start: "top 10%", 
+//     end: "top 15%", 
+//     scrub: 1,
+//     duration:10,
+//     markers: true
+//   }, 
+// });
+
+// gsap.to(".parallax-image", {
+//   xPercent: 0, 
+//   yPercent: -10,
+//   scale: 1.5,
+//   transformOrigin: "right top", 
+//   ease: "none",
+//   normalizeScroll: true,
+//   scrollTrigger: {
+//     trigger: ".parallax-image",
+//     start: "center 25%", 
+//     end: "center 60%", 
+//     scrub: 1,
+//     duration:5,
+//     markers: true
+//   }, 
+// });
+
+

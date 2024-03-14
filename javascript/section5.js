89b8efddcb8a2952748a5d37037f21f458c2e217
piiped <aomkadang@gmail.com>
2024-03-15 02:26:59 +0700

    section 6 slider brunnn

--- javascript/section5.js
+++ javascript/section5.js
@@ -35,7 +35,7 @@ gsap.set(".parallax", { scale: 1, yPercent: 0, xPercent: 0 });
 const tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".parallax",
-    scrub:3,
+    scrub:1,
     start: "top+=250px top",
     // end: "bottom+=50px bottom",
     end: "bottom+=2000",

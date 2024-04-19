0b013f373ccd108c613eb31b9505142424631050
Sarisa Sangkaranee <sarisa.skrn@gmail.com>
2024-04-20 05:38:02 +0700

    fix progress bar

--- javascript/progress.js
+++ javascript/progress.js
@@ -1,8 +1,22 @@
-scrollToHash(window.location.hash);
+// scrollToHash(window.location.hash);
 
-gsap.registerPlugin(ScrollTrigger);
-gsap.to("progress", {
-  value: 100,
-  ease: "none",
-  scrollTrigger: { scrub: 0.3 },
-});
+// gsap.registerPlugin(ScrollTrigger);
+// gsap.to("progress", {
+//   value: 100,
+//   ease: "none",
+//   scrollTrigger: { scrub: 0.3 },
+// });
+
+// When the user scrolls the page, execute pageScroll function
+window.onscroll = function () {
+  pageScroll();
+};
+
+function pageScroll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("proBar").style.width = scrolled + "%";
+}

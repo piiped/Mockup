647c7169148dc090198ccbf715eeaad6850917dd
Sarisa Sangkaranee <sarisa.skrn@gmail.com>
2024-04-20 18:41:26 +0700

    responsive all text img (no check)

--- javascript/section7.js
+++ javascript/section7.js
@@ -11,42 +11,44 @@ gsap.to(".sec-6-bg", {
   },
 });
 
-gsap.set(".h09_hand", { yPercent: -20});
+//  TODO: สร้าง timeline ปรับ motion
+
+gsap.set(".h09_hand", { yPercent: -20 });
 gsap.to(".h09_hand", {
   yPercent: 0,
   ease: "none",
   scrollTrigger: {
     trigger: ".h09_hand",
-    scrub: 1
+    scrub: 1,
   },
 });
 
-gsap.set(".h10_shirt", { yPercent: -20});
+gsap.set(".h10_shirt", { yPercent: -20 });
 gsap.to(".h10_shirt", {
   yPercent: -10,
   ease: "none",
   scrollTrigger: {
     trigger: ".h10_shirt",
-    scrub: 2
+    scrub: 2,
   },
 });
 
-gsap.set(".h05_shirt1", { yPercent: 20});
+gsap.set(".h05_shirt1", { yPercent: 20 });
 gsap.to(".h05_shirt1", {
   yPercent: -20,
   ease: "none",
   scrollTrigger: {
     trigger: ".h05_shirt1",
-    scrub: 1
+    scrub: 1,
   },
 });
 
-gsap.set(".h06_shirt2", { yPercent: 30});
+gsap.set(".h06_shirt2", { yPercent: 30 });
 gsap.to(".h06_shirt2", {
   yPercent: 0,
   ease: "none",
   scrollTrigger: {
     trigger: ".h06_shirt2",
-    scrub: 2
+    scrub: 2,
   },
 });
--- javascript/section8.js
+++ javascript/section8.js
@@ -1,46 +1,46 @@
 ScrollTrigger.create({
-    trigger: ".sec-8-container",
-    start: "top-100px top",
-    end: "bottom+=1000 top",
-    pin: ".sec-8-container",
-    ease: "power1.out",
-    scale: 0,
-    // markers:true,
-    // markers: {
-    //   startColor: "white",
-    //   endColor: "white",
-    //   fontSize: "18px",
-    //   fontWeight: "bold",
-    //   indent: 20,
-    // },
-  });
+  trigger: ".sec-8-container",
+  start: "top-100px top",
+  end: "bottom+=1000 top",
+  pin: ".sec-8-container",
+  ease: "power1.out",
+  scale: 0,
+  // markers:true,
+  // markers: {
+  //   startColor: "white",
+  //   endColor: "white",
+  //   fontSize: "18px",
+  //   fontWeight: "bold",
+  //   indent: 20,
+  // },
+});
 
-
-gsap.set(".quote_chat1", { yPercent: 10});
+//  TODO: สร้าง timeline ปรับ motion
+gsap.set(".quote_chat1", { yPercent: 10 });
 gsap.to(".quote_chat1", {
   yPercent: -10,
   duration: 2,
   scrollTrigger: {
     trigger: ".quote_chat1",
     scrub: 2,
-    start: "top 70%", 
-    end: "+=50", 
+    start: "top 70%",
+    end: "+=50",
   },
 });
 
-gsap.set(".quote_chat2", { yPercent: 30});
+gsap.set(".quote_chat2", { yPercent: 30 });
 gsap.to(".quote_chat2", {
   yPercent: -10,
   duration: 3,
   scrollTrigger: {
     trigger: ".quote_chat2",
     scrub: 2,
-    start: "top 70%", 
-    end: "+=50", 
+    start: "top 70%",
+    end: "+=50",
   },
 });
 
-gsap.set(".i05_hero", { xPercent: 0});
+gsap.set(".i05_hero", { xPercent: 0 });
 gsap.to(".i05_hero", {
   xPercent: 150,
   scrollTrigger: {
